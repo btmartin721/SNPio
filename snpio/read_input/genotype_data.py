323fcd1002a3ae656b050ac0ab8f46403c25a7df
btmartin721 <evobio721@gmail.com>
2023-04-12 10:47:16 -0500

    Changed minimum python version

--- snpio/read_input/genotype_data.py
+++ snpio/read_input/genotype_data.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import Optional, Union, List, Dict, Tuple, Any, Callable
 
 # Make sure python version is >= 3.7
-if sys.version_info < (3, 7):
+if sys.version_info < (3, 8):
     raise ImportError("Python < 3.7 is not supported!")
 
 import numpy as np
