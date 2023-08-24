5e8f1a7ee841e534398660568b04ba398f7e4f96
btmartin721 <gh_email>
2023-08-24 14:58:31 -0500

    smaller test dataset for vcfs

--- run_snpio.py
+++ run_snpio.py
@@ -12,36 +12,34 @@ def main():
     profiler.enable()
     # Read the alignment, popmap, and tree files
     gd = GenotypeData(
-        filename="example_data/vcf_files/phylogen.vcf.gz",
+        filename="example_data/vcf_files/phylogen_subset3K.vcf.gz",
         popmapfile="example_data/popmaps/phylogen_nomx.popmap",
         force_popmap=True,
         filetype="auto",
         qmatrix_iqtree="example_data/trees/test.qmat",
         siterates_iqtree="example_data/trees/test.rate",
         guidetree="example_data/trees/test.tre",
-        chunk_size=20000,
+        chunk_size=1000,
     )
     profiler.disable()
 
-    with open("example_data/vcf_files/profile_stats_readvcf.txt", "w") as f:
+    with open("./profile_stats_readvcf.txt", "w") as f:
         stats = pstats.Stats(profiler, stream=f).sort_stats("time")
         stats.print_stats()
 
-    sys.exit()
-
-    profiler = cProfile.Profile()
-    profiler.enable()
-    gd.write_vcf("example_data/vcf_files/gtdata_test.vcf")
-    profiler.disable()
-    with open("example_data/vcf_files/profile_stats_writevcf.txt", "w") as f:
-        stats = pstats.Stats(profiler, stream=f).sort_stats("time")
-        stats.print_stats()
+    #profiler = cProfile.Profile()
+    #profiler.enable()
+    # gd.write_vcf("example_data/vcf_files/gtdata_test.vcf")
+    #profiler.disable()
+    # with open("example_data/vcf_files/profile_stats_writevcf.txt", "w") as f:
+    #     stats = pstats.Stats(profiler, stream=f).sort_stats("time")
+    #     stats.print_stats()
 
     # Make missingness report plots.
-    # gd.missingness_reports(file_prefix="unfiltered")
+    gd.missingness_reports(file_prefix="unfiltered")
 
     # Run a PCA and make a scatterplot on the unfiltered data.
-    # Plotting.run_pca(gd, file_prefix="unfiltered")
+    Plotting.run_pca(gd, file_prefix="unfiltered")
 
     # Run the NRemover class to filter out missing data.
     nrm = NRemover2(gd)
@@ -57,10 +55,10 @@ def main():
     )
 
     # Make another missingness report plot for filtered data.
-    # gd_filtered.missingness_reports(file_prefix="filtered")
+    gd_filtered.missingness_reports(file_prefix="filtered")
 
     # Run a PCA on the filtered data and make a scatterplot.
-    # Plotting.run_pca(gd_filtered, file_prefix="filtered")
+    Plotting.run_pca(gd_filtered, file_prefix="filtered")
 
     gd_filtered.write_vcf("example_data/vcf_files/nremover_test.vcf")
     gd_filtered.write_phylip("example_data/phylip_files/nremover_test.phy")
