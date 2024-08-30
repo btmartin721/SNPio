33e46938976322ab8eba0b4b8046f3010a9af7d5
Bradley T. Martin <evobio721@gmail.com>
2024-08-30 12:12:17 -0400

    custum alignment file IO exceptions

--- snpio/utils/custom_exceptions.py
+++ snpio/utils/custom_exceptions.py
@@ -15,10 +15,10 @@ class UnsupportedFileTypeError(Exception):
         Args:
             filetype (str): The type of the file being checked.
             supported_types (list, optional): List of supported file types.
-                Defaults to ["vcf", "phylip", "structure", "auto"].
+                Defaults to ["vcf", "phylip", "structure"].
         """
         if supported_types is None:
-            supported_types = ["vcf", "phylip", "structure", "auto"]
+            supported_types = ["vcf", "phylip", "structure"]
 
         self.filetype = filetype
         self.supported_types = supported_types
@@ -33,3 +33,34 @@ class UnsupportedFileTypeError(Exception):
             str: The message associated with the exception.
         """
         return self.message
+    
+    
+class AlignmentError(Exception):
+    """Base class for exceptions related to alignment file handling."""
+    pass
+
+class AlignmentFormatError(AlignmentError):
+    """Raised when an alignment file has an invalid format."""
+    def __init__(self, message):
+        super().__init__(message)
+        
+class SequenceLengthError(AlignmentFormatError):
+    """Raised when sequences in the alignment file have unequal lengths."""
+    def __init__(self, sample_name):
+        super().__init__(f"Sequences have unequal lengths. Error at sample: {sample_name}")
+        
+class PhylipAlignmentSampleMismatch(AlignmentFormatError):
+    """Raised when samples in the alignment file do not match the expected number of samples."""
+    def __init__(self, header_samples, sample_list_len, snp_data_len):
+        super().__init__(f"Unexpected number of samples encountered. Expected {header_samples}, but got: {sample_list_len} samples across {snp_data_len} genotypes.")
+        
+class StructureAlignmentSampleMismatch(AlignmentFormatError):
+    """Raised when samples in the alignment file do not match the expected number of samples."""
+    def __init__(self, sample_list_len, snp_data_len):
+        super().__init__(f"Unexpected number of samples encountered. Expected {sample_list_len}, but got: {snp_data_len} genotype rows.")
+
+class AlignmentFileNotFoundError(AlignmentError):
+    """Raised when an alignment file is not found."""
+    def __init__(self, filename):
+        super().__init__(f"Alignment file '{filename}' not found.")
+
