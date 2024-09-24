080a9f748075332c7e5b269994d1819ccf419ae9
Bradley T. Martin <evobio721@gmail.com>
2024-09-24 16:05:18 -0400

    Added new XReader classes to read in input files

--- snpio/__init__.py
+++ snpio/__init__.py
@@ -1,3 +1,5 @@
-from snpio.read_input.genotype_data import GenotypeData
 from snpio.filtering.nremover2 import NRemover2
+from snpio.io.phylip_reader import PhylipReader
+from snpio.io.structure_reader import StructureReader
+from snpio.io.vcf_reader import VCFReader
 from snpio.plotting.plotting import Plotting
