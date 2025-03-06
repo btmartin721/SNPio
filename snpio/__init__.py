20cc13f00ebbaa2fd82c63967c4867f3638b957d
Tyler Chafin <tylerkchafin@gmail.com>
2025-03-06 15:32:30 +0000

    add version reporting

--- snpio/__init__.py
+++ snpio/__init__.py
@@ -6,3 +6,14 @@ from snpio.io.vcf_reader import VCFReader
 from snpio.plotting.plotting import Plotting
 from snpio.analysis.tree_parser import TreeParser
 from snpio.popgenstats.pop_gen_statistics import PopGenStatistics
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # for Python versions < 3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("snpio")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
