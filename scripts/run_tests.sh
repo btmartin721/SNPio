60c1fd77bef33bba017e526a6302c17cdb4857dc
btmartin721 <evobio721@gmail.com>
2025-05-13 15:30:28 -0400

    Simplify run_tests.sh by removing explicit Python path and pip install commands, using conda activation instead.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
-
-PYTHON_PATH="/opt/anaconda3/envs/snpio-dev/bin/python"
-
-echo "Using Python at: $PYTHON_PATH"
-
-"$PYTHON_PATH" -m pip install -e ".[dev]"
-"$PYTHON_PATH" -m pytest tests/
+source /opt/anaconda3/etc/profile.d/conda.sh
+conda activate snpio-dev
+pytest tests/
