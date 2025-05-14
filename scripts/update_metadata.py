b7cf356a7ee673aa183256a7f431def74eae3929
btmartin721 <evobio721@gmail.com>
2025-05-14 18:55:34 -0400

    CI/CD Updates

--- scripts/update_metadata.py
+++ scripts/update_metadata.py
@@ -26,7 +26,7 @@ FILES_TO_UPDATE = {
         ("version", r"(\{\\Large Version:\s*)\d+\.\d+\.\d+"),
     ],
     "scripts/tag_release.sh": [
-        ("version", r'(.*v)"\d+\.\d+\.\d+"'),
+        ("version", r'(.*\s+v)"\d+\.\d+\.\d+"'),
     ],
 }
 
