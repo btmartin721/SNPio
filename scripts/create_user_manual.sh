822375aec567747ab4cca605bd18bcb0e727a1b2
btmartin721 <evobio721@gmail.com>
2025-06-24 19:34:42 -0400

    feat: remove create_user_manual script

--- scripts/create_user_manual.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-pandoc --listings --toc -H snpio/docs/listings-setup.tex README.md --metadata-file=snpio/docs/HEADER.yaml --template=snpio/docs/template.tex --pdf-engine=xelatex -o UserManual.pdf --include-in-header=snpio/docs/admonitions.tex --lua-filter=snpio/docs/admonitions-filter.lua
